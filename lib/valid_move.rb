88a887b18dcb38f09f2a7012a97831de7881f179
Nathan Corry <n.corry@2016.ljmu.ac.uk>
2017-10-22 14:38:49 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,18 +1,3 @@
-
-def valid_move?(board, index)
-    if index.between?(0,8) && !position_taken?(board, index)
-      true
-    end
-end
-
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    false
-  else board[index] == "X" || board[index] == "O"
-    true
-  end
-end
-
 def valid_move?(board, index)
     if index.between?(0,8) && !position_taken?(board, index)
       true
